08e1ff0f53e8b134376d4864f63eeb2d7d2fe40c
pezholio <pezholio@gmail.com>
2023-05-09 16:21:00 +0100

    Refactor base pages
    There was some duplication in the Apply/Assess base pages, so I’ve
    moved some stuff to the base page.

--- pages/assess/assessPage.ts
+++ pages/assess/assessPage.ts
@@ -9,38 +9,7 @@ export class AssessPage extends BasePage {
     return new AssessPage(page)
   }
 
-  async fillField(label: string, value: string) {
-    await this.page.getByLabel(label).fill(value)
-  }
-
-  async clickSave() {
-    await this.page.getByRole('button', { name: 'Save and continue' }).click()
-  }
-
-  async checkRadio(label: string) {
-    await this.page.getByLabel(label, { exact: true }).check()
-  }
-
-  async checkRadioInGroup(group: string, label: string) {
-    await this.page
-      .getByRole('group', {
-        name: group,
-      })
-      .getByLabel(label, { exact: true })
-      .check()
-  }
-
   async checkRequirement(requirement: string, status: string) {
     await this.page.getByLabel(`${requirement} ${status}`, { exact: true }).check()
-  }
-
-  async checkCheckBoxes(labels: Array<string>) {
-    const promises = [] as Array<Promise<void>>
-
-    for (let i = 0; i < labels.length; i += 1) {
-      promises.push(this.page.getByLabel(labels[i]).dispatchEvent('click'))
-    }
-
-    await Promise.all(promises)
   }
 }
--- pages/basePage.ts
+++ pages/basePage.ts
@@ -14,4 +14,37 @@ export class BasePage {
   async clickContinue() {
     await this.page.getByRole('button', { name: 'Continue' }).click()
   }
+
+  async fillField(label: string, value: string) {
+    await this.page.getByLabel(label).fill(value)
+  }
+
+  async checkRadio(label: string) {
+    await this.page.getByLabel(label, { exact: true }).check()
+  }
+
+  async checkRadioInGroup(group: string, label: string) {
+    await this.page
+      .getByRole('group', {
+        name: group,
+      })
+      .getByLabel(label, { exact: true })
+      .check()
+  }
+
+  async checkCheckBoxes(labels: Array<string>) {
+    const promises = [] as Array<Promise<void>>
+
+    for (let i = 0; i < labels.length; i += 1) {
+      promises.push(this.page.getByLabel(labels[i]).dispatchEvent('click'))
+    }
+
+    await Promise.all(promises)
+  }
+
+  async fillDateField({ year, month, day }: { year: string; month: string; day: string }) {
+    await this.page.getByLabel('Day').fill(day)
+    await this.page.getByLabel('Month').fill(month)
+    await this.page.getByLabel('Year').fill(year)
+  }
 }
