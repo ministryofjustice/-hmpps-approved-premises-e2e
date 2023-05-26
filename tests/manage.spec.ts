f64753b5f984237e14a80b042dafa2f881f5a440
rich <rich.james@dxw.com>
2023-05-26 10:26:20 +0100

    Extract functionality to manually book a bed
    These test will be running against the dev env DB which is cleared
    on every deploy of the API.
    We can’t guarantee that the DB will have bookings against beds so before
    cancelling, marking an arrival or non-arrival we need to ensure there
    is a booking. Because we will be repeating this functionality it’s
    best to extract it to seperate functions.

--- tests/manage.spec.ts
+++ tests/manage.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test'
 import { visitDashboard } from '../steps/apply'
 import { PremisesListPage } from '../pages/manage/premisesListPage'
 import { PremisesPage } from '../pages/manage/premisesPage'
+import { PlacementPage } from '../pages/manage/placementPage'
 import { BedsPage } from '../pages/manage/bedsPage'
 import { BedPage } from '../pages/manage/bedPage'
 import { ConfirmationPage } from '../pages/manage/placementConfirmationPage'
@@ -9,8 +10,24 @@ import { CRNPage } from '../pages/apply'
 import { CreatePlacementPage } from '../pages/manage/createPlacementPage'
 import { MarkBedOutOfServicePage } from '../pages/manage/markBedOutOfServicePage'
 
-test('Manually book a bed', async ({ page }) => {
-  const premisesName = 'Test AP 1'
+const premisesName = 'Test AP 1'
+
+const navigateToBedsPage = async page => {
+  await navigateToPremisesPage(page)
+
+  // Then I should see the premises view page
+  const premisesPage = await PremisesPage.initialize(page, premisesName)
+
+  // Given I am on an individual Premises' page
+  // When I click the 'View Rooms' link
+  await premisesPage.viewRooms()
+
+  // Then I should see the rooms view page
+  const bedsPage = await BedsPage.initialize(page, 'Manage beds')
+  return bedsPage
+}
+
+const navigateToPremisesPage = async page => {
   // Given I visit the dashboard
   const dashboard = await visitDashboard(page)
 
@@ -22,20 +39,53 @@ test('Manually book a bed', async ({ page }) => {
 
   // When I click on a Premises' 'View' link
   await listPage.choosePremises(premisesName)
+}
 
-  // Then I should see the premises view page
+const navigateToTodaysBooking = async page => {
+  await navigateToPremisesPage(page)
+
   const premisesPage = await PremisesPage.initialize(page, premisesName)
 
-  // Given I am on an individual Premises' page
-  // When I click the 'View Rooms' link
-  await premisesPage.viewRooms()
+  await premisesPage.clickManageTodaysArrival()
+}
 
-  // Then I should see the rooms view page
-  const bedsPage = await BedsPage.initialize(page, 'Manage beds')
+const manuallyBookBed = async page => {
+  const bedsPage = await navigateToBedsPage(page)
 
   // Given I am on the rooms view page
   // When I click the 'Manage' link for a room
-  await bedsPage.viewBed()
+  await bedsPage.viewAvailableBed()
+
+  // Then I should see the room view page
+  const bedPage = await BedPage.initialize(page, 'Manage beds')
+
+  // And be able to select a bed
+  await bedPage.clickBookBed()
+
+  // Given I am on the CRN entry page
+  const crnPage = new CRNPage(page)
+  // When I enter a CRN
+  await crnPage.enterCrn()
+  await crnPage.clickSearch()
+
+  // Then I should see the placement page
+  // Given I am on the placement page
+  const createPlacementPage = new CreatePlacementPage(page)
+  // When I complete the form
+  await createPlacementPage.completeForm()
+  createPlacementPage.clickSubmit()
+
+  // Then I should be taken to the confirmation page
+  const confirmationPage = new ConfirmationPage(page)
+  await confirmationPage.shouldShowSuccessMessage()
+}
+
+test('Manually book a bed', async ({ page }) => {
+  const bedsPage = await navigateToBedsPage(page)
+
+  // Given I am on the rooms view page
+  // When I click the 'Manage' link for a room
+  await bedsPage.viewAvailableBed()
 
   // Then I should see the room view page
   const bedPage = await BedPage.initialize(page, 'Manage beds')
