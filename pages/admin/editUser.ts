e0372aa2fd6616477b4b157da29bf04a2d6c156f
rich <rich.james@dxw.com>
2023-09-13 09:51:08 +0100

    Add tests for user management feature
    I have added one test here to test all the functionality in a light-touch manner.
    It is slightly risky to delete a user here but I have opted to add them first
    so that if the test fails at any point then when it is run again it should work.
    There is no issue with adding a user that already exists - the functionality works the same.
    I have opted to use JOSEPHHOLLINSHEAD just in case the user is deleted and we are unable to recover
    the account for some unknown reason.
    For the search functionality we assert a user that appears later in the list (so they
    are not visible in the first page of results) isnt visible. We then search for the user and ensure
    they are visible.

---
+++ pages/admin/editUser.ts
@@ -0,0 +1,28 @@
+import { Page, expect } from '@playwright/test'
+import { BasePage } from '../basePage'
+
+export class EditUser extends BasePage {
+  static async initialize(page: Page) {
+    await expect(page.locator('h1')).toContainText('Manage permissions')
+
+    return new EditUser(page)
+  }
+
+  async clickSave(): Promise<void> {
+    await this.page.getByRole('button', { name: 'Save' }).click()
+  }
+
+  async clickRemoveAccess(): Promise<void> {
+    await this.page.getByRole('button', { name: 'Remove access' }).click()
+  }
+
+  async shouldShowUserUpdatedBanner() {
+    await expect(this.page.locator('h3')).toContainText('User updated')
+  }
+
+  async shouldShowUserName(username: string) {
+    await expect(this.page.locator('h1')).toContainText('Manage permissions')
+    await expect(this.page.getByRole('definition')).toHaveCount(5)
+    this.page.getByRole('definition', { name: username })
+  }
+}
--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -2,6 +2,11 @@ import { expect } from '@playwright/test'
 import { test } from '../test'
 import { visitDashboard } from '../steps/apply'
 import { ReportsPage } from '../pages/admin/reports'
+import { UserList } from '../pages/admin/listUsers'
+import { AddUser } from '../pages/admin/addUser'
+import { EditUser } from '../pages/admin/editUser'
+import { NewUserConfirmationPage } from '../pages/admin/newUserConfirmationPage'
+import { DeleteUserConfirmationPage } from '../pages/admin/deleteUserConfirmationPage'
 
 test('download reports', async ({ page }) => {
   // Given I visit the dashboard
@@ -25,4 +30,69 @@ test('download reports', async ({ page }) => {
   const applicationsDownload = await reportsPage.downloadApplicationsReports({ month, year })
   // Then the file should be downloaded with the correct suggested name
   expect(applicationsDownload.suggestedFilename()).toMatch(`applications-${year}-${month}.xlsx`)
+})
+
+test('manage users', async ({ page }) => {
+  const userToDeleteAndAdd = 'JOSEPHHOLLINSHEAD'
+
+  // Given I visit the dashboard
+  const dashboard = await visitDashboard(page)
+
+  // When I click the manage users link
+  await dashboard.clickUserMangement()
+
+  // Then I should be taken to user list page
+  const userListPage = await UserList.initialize(page)
+
+  // When I click the add user link
+  await userListPage.clickAddUser()
+
+  // Then I should be taken to the add user page
+  const addUserPage = await AddUser.initialize(page)
+
+  // When I search for a user
+  await addUserPage.search(userToDeleteAndAdd)
+
+  // Then I should be taken to the confirmation page
+  const newUserConfirmationPage = await NewUserConfirmationPage.initialize(page)
+
+  // When I click continue
+  await newUserConfirmationPage.clickContinue()
+
+  // Then I should be taken to the Edit User page
+  const editUserPage = await EditUser.initialize(page)
+
+  // When I click 'Save'
+  await editUserPage.clickSave()
+
+  // Then I should see the 'User updated' banner
+  await editUserPage.shouldShowUserUpdatedBanner()
+
+  // When I click 'Remove access'
+  await editUserPage.clickRemoveAccess()
+
+  // Then I should be taken to the confirmation page
+  const deleteUserConfirmationPage = await DeleteUserConfirmationPage.initialize(page)
+
+  // When I click 'Remove access'
+  await deleteUserConfirmationPage.clickRemoveAccess()
+
+  // Then I should see the 'User deleted' banner
+  await userListPage.shouldShowUserDeletedBanner()
+
+  // Given a user is not visisble in the list
+  const userToSearchFor = 'AutomatedTestUser'
+  await userListPage.shouldNotShowUser(userToSearchFor)
+
+  // When I search for them
+  await userListPage.search(userToSearchFor)
+
+  // Then I should be able to see their name
+  await userListPage.shouldShowUser(userToSearchFor)
+
+  // When I click on their name
+  await userListPage.clickEditUser(userToSearchFor)
+
+  // Then I should be taken to the Edit User page
+  await editUserPage.shouldShowUserName(userToSearchFor)
 })
