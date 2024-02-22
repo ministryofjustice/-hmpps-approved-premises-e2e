1a1e84c7d9773d052ecaf38b413c2ccdf962b9c5
rich <rich.james@dxw.com>
2024-02-22 12:49:46 +0000

    Remove field
    This has been removed from the UI so needs to be removed from the tests

--- pages/apply/appealApplicationPage.ts
+++ pages/apply/appealApplicationPage.ts
@@ -4,7 +4,6 @@ export class AppealApplicationPage extends BasePage {
   async fillForm() {
     await this.fillDateField({ year: '2023', month: '3', day: '12' })
     await this.fillField('What was the reason for the appeal?', 'Appeal reasons')
-    await this.fillField('Who made the decision on the appeal?', 'Appeal staff details')
     await this.checkRadio('Upheld')
     await this.fillField('What are the reasons for the appeal decision?', 'Appeal decision reasons')
     await this.clickSave()
